a32810236fa552609740ee5f35fb9ce819ef9ed1
Raul Armenta Urrutia <armentaur.raul@gmail.com>
2020-09-25 19:53:38 -0500

    fix: 🐛 cambio de conexion de base de datos a jdbc

--- app/index.ts
+++ app/index.ts
@@ -3,16 +3,23 @@ import { buildSchema, Int } from "type-graphql";
 import logger from "./helpers/logins/login.helper";
 import BadRequestException from "./exceptions/bad-request.exception";
 import { SchemaOptions } from "./resolvers";
-
-import { Pool } from "pg";
+import jinst from 'jdbc/lib/jinst';
+import Pool from 'jdbc/lib/pool';
 import { ExpirationStrategy, MemoryStorage } from "node-ts-cache";
-const client = new Pool({
+
+if (!jinst.isJvmCreated()) {
+  jinst.addOption("-Xrs");
+  jinst.setupClasspath([`${__dirname}/../drivers/*.jar`]);
+}
+
+var config = {
+  url: process.env.jdbc_url,
   user: process.env.db_user,
-  database: process.env.db_database,
-  port: process.env.db_port,
   password: process.env.db_password,
-  host: process.env.db_host,
-});
+  minpoolsize: 2,
+  maxpoolsize: 20
+};
+
 const myCache = new ExpirationStrategy(new MemoryStorage());
 //Funcion principal del proyecto
 const init = async (port: any) => {
@@ -21,17 +28,8 @@ const init = async (port: any) => {
   const schema = await buildSchema(SchemaOptions);
   //La coneccion a base
   logger.info(`Connectiong databases`);
-  let connection: Pool;
-
-  await client
-    .connect()
-    .then((e) => {
-      logger.info(`Connectiong Exist to Redshift`);
-      connection = e;
-    })
-    .catch((e) => {
-      logger.error("connection DWH failed", e);
-    });
+  let connection = new Pool(config);
+  
   //Se Crea el server
   logger.info(`Initializing server`);
   const server = new ApolloServer({
