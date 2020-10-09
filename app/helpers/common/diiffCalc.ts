6aa825755eeb7d7594b3b8604a844869eb04623c
Raul Armenta Urrutia <armentaur.raul@gmail.com>
2020-10-09 13:14:39 -0500

    fix: 🐛 infinite para calculo de diff

--- app/helpers/common/diiffCalc.ts
+++ app/helpers/common/diiffCalc.ts
@@ -1,4 +1,4 @@
 export const diffCalc = (act, prev) => {
   let diff = Math.round(((prev * 100) / act) * -1 + 100);
-  return isNaN(diff) ? 0 : diff;
+  return isNaN(diff) || !isFinite(diff) ? 0 : diff;
 };
